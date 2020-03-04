e32ecb2286a0531ce9c2deb68366c283a27a1da8
kycfeel <kycfeel@gmail.com>
2020-03-04 15:24:06 +0900

    'hollaex prod' command improvement

--- tools_generator.sh
+++ tools_generator.sh
@@ -4262,6 +4262,36 @@ EOF
 
 }
 
+function hollaex_ascii_exchange_has_been_ready_for_prod() {
+
+  /bin/cat << EOF
+
+1ttffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffttt.
+.@@@000000000000000000000000000000000000000000000000000000000000000000@@@,
+.0@G                                                                  L@8,
+.8@G     fLL:  ;LLt         ;00L:00C         ;LfLCCCC;                C@@,
+.8@G    .@@@;  i@@8  :1fti, i@@G;@@0 ,ittti, t@@0ttfL1ttt..ttt,       C@@,
+.8@G    .8@@0GG0@@G:0@@LG@@f;@@C;@@0.L00L8@@;1@@0LL.  t@@CC@@1        C@@,
+.8@G    .8@@LttC@@GC@@t  8@@f@@C;@@G:LGCtG@@1i@@Gtt    1@@@8:         C@8,
+.8@G    .@@@;  i@@0i@@81L@@Ci@@G;@@0f@@G10@@t1@@8ffLL1i8@C0@8;.1t;    C@@,
+.8@G     tff,  :fft ,1LCCf; ,ff1,fft.1LCL1ff;:fffLLLf;fff ,fLf,;i:    ;ii.
+.0@G
+.@@@888888888888888888888888888888888888888888888888888888888888888888880.
+1ttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt.
+
+              Your Exchange has been setup for production!
+    Please run 'hollaex restart$(if [[ "$USE_KUBERNETES" ]]; then echo " --kube"; fi)' & 'hollaex web --restart$(if [[ "$USE_KUBERNETES" ]]; then echo " --kube"; fi)'
+    to apply the changes you made.
+
+    For the web, You should rebuild the Docker image to apply the changes.
+
+    Have fun <3!
+
+                 
+EOF
+
+}
+
 function hollaex_ascii_exchange_has_been_stopped() {
 
   /bin/cat << EOF
