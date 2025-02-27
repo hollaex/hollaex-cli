9bdfe8d47e2b387c8f335db5f63eab59fb9c1772
Yechan Kim <kycfeel@gmail.com>
2025-02-28 08:53:36 +0900

    5 -> 10 mins

--- tools_generator.sh
+++ tools_generator.sh
@@ -219,7 +219,7 @@ function kubernetes_database_init() {
         ELAPSED_TIME=$(($(date +%s) - $START_TIME))
         
         if [ $ELAPSED_TIME -ge $TIMEOUT ]; then
-            echo "Timeout reached! The job did not complete in 5 minutes."
+            echo "Timeout reached! The job did not complete in 10 minutes."
             exit 1
         fi
         
